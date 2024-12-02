f625bc99574c8e32eff03364f976c028eae56efc
Gabriel Duarte <gabrieldvr@outlook.com>
2024-12-02 17:08:36 -0300

    iniciando frontend com vite, instalando dependências e fazendo algumas modificações pro projeto também rodar no macOS

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Addressly</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/vite.config.ts
@@ -0,0 +1,28 @@
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tailwindcss from "tailwindcss";
+
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  preview: {
+    port: 5173,
+    strictPort: true,
+  },
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:5173",
+  },
+});
